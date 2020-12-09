0a7ed79bd6035f80191348aa8563f7e329bf0e87
Shelby <shelbyrobison14@gmail.com>
2020-12-09 13:36:01 -0500

    siffon outline font is working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -49,7 +49,7 @@ router.put("/api/burger/:id", function(req, res) {
 router.delete("/api/burger/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burgers.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
